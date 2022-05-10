aba9e948239530f8723435421ce69ae039b38d7b
John Francis <jean_f_05@hotmail.com>
2022-05-10 14:29:18 +0100

    added hidden class and listeners

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const mainBtn = document.querySelector(".mainBtn");
 console.log(mainBtn);
+const cardMain = document.querySelector(".card__main ");
+
+mainBtn.addEventListener("click", () => {
+  mainBtn.classList.add("hidden");
+  cardMain.classList.remove("hidden");
+});
