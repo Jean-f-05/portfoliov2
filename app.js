58a5935ff8d6deceb45d0607d61fd733403003fc
John Francis <jean_f_05@hotmail.com>
2022-05-11 13:24:25 +0100

    added text animation/keyframe/js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const mainBtn = document.querySelector(".mainBtn");
 const cardMain = document.querySelector(".card__main ");
+// const cardText = document.querySelectorAll(".card__text")[0].children;
+const cardText = document.getElementsByClassName("card__text__el");
+
+console.log(typeof cardText);
 
 mainBtn.addEventListener("click", () => {
   mainBtn.classList.add("hide");
   setTimeout(() => {
     cardMain.classList.remove("hidden");
+    Array.from(cardText).forEach((element) => {
+      element.classList.add("textIn");
+    });
   }, 2000);
 });
+
+set;
