c317e7aca0c54d2efa67d9f664a08b0bacaa0c0b
John Francis <jean_f_05@hotmail.com>
2022-05-12 15:40:42 +0100

    added main empty JS

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cardMain = document.querySelector(".card__main ");
 // const cardBtn = document.querySelector(".card__btn");
 const cardBtn = document.querySelector(".card__main__el");
 
+const mainComponent = document.querySelector("main");
+console.log(mainComponent);
+
 const cardText = document.getElementsByClassName("card__text__el");
 const cardImg = document.querySelector(".card__img");
 const cardBg = document.querySelector(".card__main__bg");
@@ -19,4 +22,8 @@ mainBtn.addEventListener("click", () => {
       cardBtn.classList.add("cardBtnIn");
     });
   }, 2000);
+});
+
+cardBtn.addEventListener("click", () => {
+  mainComponent.innerHTML = "";
 });
